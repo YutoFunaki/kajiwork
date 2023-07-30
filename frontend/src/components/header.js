20b9565b9465c3b9c2985001a61b73c9452d5f20
YutoFunaki <kaizuka0629@gmail.com>
2023-07-30 18:14:06 +0900

    アカウントアイコン作成

--- frontend/src/components/header.js
+++ frontend/src/components/header.js
@@ -1,12 +1,18 @@
-//import React, { useState } from "react";
+//import React, { useContext } from "react";
 import "./MyComponent.css";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import ListIcon from '@mui/icons-material/List';
 
 const Header = () => {
   return (
     <div className="header">
+      <ListIcon className="ListIcon" />
       <p className="AppTitle">
         KajiWork
       </p>
+      <div className="AccountCircleIconBackground">
+        <AccountCircleIcon className="AccountCircleIcon" />
+      </div>
     </div>  
   );
 };
