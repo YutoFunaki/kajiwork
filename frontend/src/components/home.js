feafbd89dea767d3159500269335514187ea4c98
YutoFunaki <kaizuka0629@gmail.com>
2023-08-01 19:59:52 +0900

    カレンダーでエラー： 'handleDateClick' is not defined

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -2,8 +2,14 @@
 //import {Link} from 'react-router-dom';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from "@fullcalendar/interaction";
+import "./MyComponent.css";
 
 const Home = () => {
+
+  handleDateClick = (arg) => { // bind with an arrow function
+    alert(arg.dateStr)
+  }
 
   return (
     <div className="homestyle">
@@ -12,9 +18,16 @@ const Home = () => {
       </div>
       <div className="homeright">
       <FullCalendar
-       plugins={[dayGridPlugin]}
+       plugins={[dayGridPlugin, interactionPlugin]}
+       dateClick={this.handleDateClick}
         initialView="dayGridMonth"
-        locale={"ja"} />
+        locale={"ja"} 
+        weekends={false}
+        events={[
+          {title: '家賃', date: '2023-08-01'},
+          {title: '光熱費', date: '2023-08-01'},
+        ]}
+      />
       </div>
 
     </div>
