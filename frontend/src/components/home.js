1e2d36f8c6c710e1e6d9907583d47e15d9fd0945
YutoFunaki <kaizuka0629@gmail.com>
2023-08-02 01:23:39 +0900

    明日左側整える

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -9,7 +9,15 @@ const Home = () => {
   return (
     <div className="homestyle">
       <div className="homeleft">
-        <p className="">7月の生活費</p>
+        <p className="monthMoney">7月の生活費(確定)</p>
+        <p className="lastMonthMoney">あなた：　○○○円</p>
+        <p className="lastMonthMoney">れな：　○○○円</p>
+        <p className="monthMoney">8月の生活費(未確定)</p>
+        <p className="nowMonthMoney">あなた：　○○○円</p>
+        <p className="nowMonthMoney">れな：　○○○円</p>
+        <p className="todayCompleteWork">2023/7/6に行った家事</p>
+        <p className="todayCompleteWorktitle">あなた：</p>
+        <p className="todayCompleteWorktitle">れな：</p>
       </div>
       <div className="homeright">
        <Calendarcomponent />
