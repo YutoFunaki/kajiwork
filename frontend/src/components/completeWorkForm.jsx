d920043f6b5af784be2c4241498d63d5232b93b5
YutoFunaki <kaizuka0629@gmail.com>
2023-08-02 17:20:30 +0900

    handleCheckboxChengeを修正

--- frontend/src/components/completeWorkForm.jsx
+++ frontend/src/components/completeWorkForm.jsx
@@ -2,45 +2,39 @@ import React, { useState } from "react";
 
 const CompleteWorkForm = () => {
   const [selectedUser, setSelectedUser] = useState("");
-  const [selectedWork, setSelectedWork] = useState("");
-  // セレクトボックスの選択肢
+  const [selectedWork, setSelectedWork] = useState([]); // 初期値として空の配列を設定
+
   const options = [
     { value: "you", label: "あなた" },
     { value: "rena", label: "れな" },
   ];
 
   const works = [
-    { value: "you", label: "洗い物" },
-    { value: "rena", label: "れな" },
+    { value: "washing", label: "洗い物" },
+    { value: "cleaning", label: "掃除" },
   ];
 
-  // セレクトボックスの変更時に呼び出される関数
-  const handleSelectChange = (user, work) => {
-    setSelectedUser(user.target.value);
-    setSelectedWork(work.target.value);
+  const handleSelectChange = (event) => {
+    setSelectedUser(event.target.value);
   };
 
-    // 選択されたチェックボックスの値を更新する関数
-    const handleCheckboxChange = (event) => {
-      const value = event.target.value;
-      if (selectedWork.includes(value)) {
-        setSelectedWork(selectedWork.filter((work) => work !== value));
-      } else {
-        setSelectedWork([...selectedWork, value]);
-      }
-    };
+  const handleCheckboxChange = (event) => {
+    const value = event.target.value;
+    if (selectedWork.includes(value)) {
+      setSelectedWork(selectedWork.filter((work) => work !== value));
+    } else {
+      setSelectedWork([...selectedWork, value]);
+    }
+  };
 
-  
-  // 送信ボタンがクリックされたときに呼び出される関数
-  //const handleSubmit = (user) => {
-  //  user.preventDefault();
-  //  console.log(selectedUser);
-  //};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(selectedUser, selectedWork);
+  };
 
   return (
     <div>
       <p className="AppSubtitle">家事完了ページ</p>
-      {/* セレクトボックス */}
 
       <div className="completeForm">
         <label className="userSelect">
@@ -53,33 +47,31 @@ const CompleteWorkForm = () => {
             ))}
           </select>
         </label>
-      <div>
-        {works.map((work) => (
-        <label key={work.value}>
-          <input
-            type="checkbox"
-            value={work.value}
-            checked={selectedWork.includes(work.value)}
-            onChange={handleCheckboxChange}
-          />
-          {work.label}
-        </label>
-      ))}
-      </div>
-      <div>
-        <p>選択されたオプション:</p>
-        <ul>
-          {selectedWork.map((work) => (
-            <li key={work}>{work}</li>
+        <div>
+          {works.map((work) => (
+            <label key={work.value}>
+              <input
+                type="checkbox"
+                value={work.value}
+                checked={selectedWork.includes(work.value)}
+                onChange={handleCheckboxChange}
+              />
+              {work.label}
+            </label>
           ))}
-        </ul>
+        </div>
+        <div>
+          <p>選択されたオプション:</p>
+          <ul>
+            {selectedWork.map((work) => (
+              <li key={work}>{work}</li>
+            ))}
+          </ul>
+        </div>
       </div>
-      </div>
-      {/* 送信ボタン */}
-     {/*<button className="LoginButton" onClick={handleSubmit}>送信</button>*/}
+      <button className="LoginButton" onClick={handleSubmit}>送信</button>
     </div>
   );
 };
 
 export default CompleteWorkForm;
-
