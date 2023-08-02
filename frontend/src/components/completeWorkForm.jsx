ccd984cd11d1b5a1071bbc49c29475d5f6565bc7
YutoFunaki <kaizuka0629@gmail.com>
2023-08-02 16:11:21 +0900

    セレクトボックスの追加

--- frontend/src/components/completeWorkForm.jsx
+++ frontend/src/components/completeWorkForm.jsx
@@ -1,35 +1,42 @@
 import React, { useState } from "react";
-import Select from "react-select";
 
 const CompleteWorkForm = () => {
-  const users = [
+  const [selectedUser, setSelectedUser] = useState("");
+
+  // セレクトボックスの選択肢
+  const options = [
     { value: "you", label: "あなた" },
     { value: "rena", label: "れな" },
   ];
 
-  const [selectedValue, setSelectedValue] = useState(users[0]);
+  // セレクトボックスの変更時に呼び出される関数
+  const handleSelectChange = (user) => {
+    setSelectedUser(user.target.value);
+  };
+
+  // 送信ボタンがクリックされたときに呼び出される関数
+  const handleSubmit = (user) => {
+    user.preventDefault();
+    console.log(selectedUser);
+  };
 
   return (
     <div>
-      <p className="AppSubtitle">家事完了ページ</p>
-
-      <div className="userSelect">
-        <Select
-          options={users}
-          value={selectedValue}
-          onChange={(value) => {
-            setSelectedValue(value);
-          }}
-        />
-      </div>
-
-      <label className="workSelect">
-        <select className="item" multiple>
-          <option>text1</option>
-          <option>text2</option>
-          <option>text3</option>
+      <h1>セレクトボックスの例</h1>
+      {/* セレクトボックス */}
+      <label className="userSelect">
+        <select value={selectedUser} onChange={handleSelectChange}>
+          <option value="">選択してください</option>
+          {options.map((option) => (
+            <option key={option.value} value={option.value}>
+              {option.label}
+            </option>
+          ))}
         </select>
       </label>
+
+      {/* 送信ボタン */}
+      <button onClick={handleSubmit}>送信</button>
     </div>
   );
 };
