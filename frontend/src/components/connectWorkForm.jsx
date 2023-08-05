8a1c160e0de55ac016b09014c9cb37c49ce45f42
YutoFunaki <kaizuka0629@gmail.com>
2023-08-05 16:51:24 +0900

    文字追加

--- frontend/src/components/connectWorkForm.jsx
+++ frontend/src/components/connectWorkForm.jsx
@@ -1,12 +1,24 @@
 import React from "react";
 //import ko from "knockout";
 import "./MyComponent.css";
-//import { useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const ConnectWorkForm = () => {
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    navigate("/home");
+  };
+
   return (
     <div>
       <p className="AppSubtitle">送信完了しました！お疲れ様でした！</p>
+      <p>あなた</p>
+      <p>完了した家事：</p>
+      <p>今回の家事合計：</p>
+
+      <button className="LoginButton" onClick={handleSubmit}>送信</button>
     </div>
   );
 };
