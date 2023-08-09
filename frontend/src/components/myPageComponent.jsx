a57f7ccf29ab7e5a3cd0e10495e4b15509deaa85
YutoFunaki <kaizuka0629@gmail.com>
2023-08-09 19:32:54 +0900

    ミス

--- frontend/src/components/myPageComponent.jsx
+++ frontend/src/components/myPageComponent.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const MyPageComponent = () => {
   return (
     <div>
-      <p className="AppSubtitle">マイページ<i></i></p>
+      <p className="AppSubtitle">マイページ</p>
     </div>
   );
 }
