dd272ee6dd816be9c8b719d08f84ef50f76d15e0
YutoFunaki <kaizuka0629@gmail.com>
2023-09-11 23:53:42 +0900

    名前や月の生活費を変更できるようにした

--- frontend/src/components/myPageComponent.jsx
+++ frontend/src/components/myPageComponent.jsx
@@ -6,6 +6,34 @@ const getCookie = (name) => {
   if (parts.length === 2) return parts.pop().split(';').shift();
 };
 
+const ChangeAPI = async (username, personname, lifemoney, inputUsername, inputPersonname, inputLifemoney, room_id, nav) => {
+  await fetch('http://localhost:8080/api/changepersonnal', {
+    method: 'POST',
+    mode: 'cors',
+    // credentials: 'include',
+    headers: {
+      "Content-Type": "application/json",
+
+      // 'Content-Type': 'application/x-www-form-urlencoded',
+    },
+    body: JSON.stringify({"username": username, "personname": personname, "lifemoney": lifemoney, "inputUsername": inputUsername, 'inputPersonname': inputPersonname, 'inputLifemoney': inputLifemoney, 'room_id': room_id}),
+  }) 
+  .then(async response => {
+    // 成功
+    if (response.status === 200) {
+      console.log("成功 : " + response.status);
+      const userData = await response.json();
+      const username = userData.username;
+      const personname = userData.personname;
+      document.cookie = `username=${username}`
+      document.cookie = `personname=${personname}`
+      window.location.reload()
+    } else if(response.status === 401) {
+      console.log('失敗 : ' + response.status)
+      alert("ユーザー名またはメールアドレスが既に登録されています。");
+    }
+  }) //2
+}
 
 const MyPageComponent = () => {
   const room_id = getCookie('room_id');
@@ -13,6 +41,9 @@ const MyPageComponent = () => {
   const personname = getCookie('personname');
   const [userData, setUserData] = useState(null);
   const [lifemoney, setLifemoney] = useState(null);
+  const [inputUsername, setInputUsername] = useState("");
+  const [inputPersonname, setInputPersonname] = useState("");
+  const [inputLifemoney, setInputLifemoney] = useState("");
 
   useEffect(() => {
     // APIからデータを取得する
@@ -34,13 +65,60 @@ const MyPageComponent = () => {
   }, []); 
 
 
+  const handleChangeSubmit = async() => {
+    console.log(username);
+    console.log(inputUsername);
+    console.log(personname);
+    console.log(inputPersonname);
+    console.log(lifemoney);
+    console.log(inputLifemoney);
+    await ChangeAPI(username, personname, lifemoney, inputUsername, inputPersonname, inputLifemoney, room_id);
+  };
+
   return (
     <div>
       <p className="AppSubtitle">マイページ</p>
-      <p>ユーザー名：{username}</p>
-      <p>パートナー名：{personname}</p>
-      <p>ルームID：{room_id}</p>
-      <p>家計簿：{lifemoney}</p>
+      <div className="mypage">
+        <div className="inputcontainer">
+          <p>ユーザー名：{username}</p>
+          <input
+            placeholder="変更する場合は入力"
+            type="text"
+            className="inputPersonalData"
+            id="username"
+            value={inputUsername}
+            onChange={(e) => setInputUsername(e.target.value)} />
+        </div>
+        
+        <div className="inputcontainer">
+          <p>パートナー名：{personname}</p>
+          <input
+            placeholder="変更する場合は入力"
+            type="text"
+            className="inputPersonalData"
+            id="personname"
+            value={inputPersonname}
+            onChange={(e) => setInputPersonname(e.target.value)} />
+        </div>
+      
+        <div className="inputcontainer">
+          <p>月の生活費：{lifemoney}</p>
+          <input
+            placeholder="変更する場合は入力"
+            type="text"
+            className="inputPersonalData"
+            id="lifemoney"
+            value={inputLifemoney}
+            onChange={(e) => setInputLifemoney(e.target.value)} />
+        </div>
+        
+        </div>
+        <button
+                type="button"
+                className="LoginButton"
+                onClick={handleChangeSubmit}
+              >変更
+            </button>
     </div>
   );
 }
