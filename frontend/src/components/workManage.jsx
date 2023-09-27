2f1930b23580342a7c7c952cc2c2d527f638186f
YutoFunaki <kaizuka0629@gmail.com>
2023-09-27 21:36:56 +0900

    ロード画面追加

--- frontend/src/components/workManage.jsx
+++ frontend/src/components/workManage.jsx
@@ -10,6 +10,7 @@ const getCookie = (name) => {
 const ReWorkAPI = async (selectedWork, inputWorkname, frequency, room_id, nav) => {
   console.log("送信するデータ");
   console.log(selectedWork);
+  console.log(inputWorkname);
 
   // 非同期処理
   await fetch('http://localhost:8080/api/workrename', {
@@ -30,7 +31,6 @@ const ReWorkAPI = async (selectedWork, inputWorkname, frequency, room_id, nav) =
       console.log("受け取ったデータ");
       console.log(data);
       console.log("成功 : " + response.status);
-      window.location.reload();
     } else if(response.status === 401) {
       console.log('失敗 : ' + response.status)
       alert("エラーが発生しました。");
@@ -56,7 +56,6 @@ const DeleteWorkAPI = async (selectedWork) => {
     // 成功
     if (response.status === 200) {
       console.log("成功 : " + response.status);
-      window.location.reload(); 
     } else if(response.status === 401) {
       console.log('失敗 : ' + response.status)
       alert("エラーが発生しました。");
@@ -65,6 +64,7 @@ const DeleteWorkAPI = async (selectedWork) => {
 };
 
 const WorkManage = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const room_id = getCookie('room_id');
   const [userData, setUserData] = useState(null);
   const [workname, setWorkname] = useState([]);
@@ -78,36 +78,56 @@ const WorkManage = () => {
   const nav = useNavigate();
 
   useEffect(() => {
-    // APIからデータを取得する
-    async function fetchData() {
-      try {
-        const response = await fetch(`http://localhost:8080/api/workmanage?room_id=${room_id}`, {
-          method: 'GET',
-          mode: 'cors',
-          credentials: 'include',
-          headers: {
-            "Content-Type": "application/json"
-          },
-        });
-        const data = await response.json();
+    getData();
+  }, []); 
+  
+
+  const getData = () => {
+    fetch('http://localhost:8080/api/workmanage', {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include', 
+      headers: {
+        'Content-Type': 'application/json', 
+      },
+    })
+      .then(response => response.json()) 
+      .then(data => {
         setUserData(data);
         setWorkname(data.tasks_name);
         setFrequency(data.tasks_frequency);
         setTask_id(data.tasks_id);
         console.log(data);
-      } catch (error) {
-        console.error('データの取得に失敗しました', error);
-      }
+      })
+      .catch(error => {
+        console.error('エラー:', error);
+      });
     }
-
-    fetchData();
-  }, []);
-
 
 
   const handleSigninSubmit = async() => {
-    console.log("ReWorkAPI 関数が呼び出されました");
-    await ReWorkAPI(selectedWork, inputWorkname, refrequency, room_id, nav);
+    setIsLoading(true); // ローディングを有効
+    
+    try {
+      const selectedWorkItem = formatData.find((item) => item.value === selectedWork);
+  
+      if (selectedWorkItem) {
+        
+        // API呼び出しを非同期で実行
+        await ReWorkAPI(selectedWork, inputWorkname, refrequency, room_id, nav);
+        getData();
+
+        setInputWorkname("");
+        setSelectedWork([]);
+      } else {
+        alert("家事を選択してください。");
+      }
+    } catch (error) {
+      console.error("APIエラー:", error);
+      // エラー処理
+    } finally {
+      setIsLoading(false); // ローディングを無効
+    }
   };
 
 
@@ -139,15 +159,43 @@ const WorkManage = () => {
   
 
   const handleDeleteSubmit = async() => {
-    console.log("DeleteWorkAPI 関数が呼び出されました");
-    await DeleteWorkAPI(selectedWork);
+    setIsLoading(true); // ローディングを有効
+    
+    try {
+      const selectedWorkItem = formatData.find((item) => item.value === selectedWork);
+    
+    if (selectedWorkItem) {
+      await DeleteWorkAPI(selectedWork);
+      getData();
+      //データを送るとselectedworkをリセット
+      setSelectedWork([]);
+      } else {
+        alert("家事を選択してください。");
+      }
+    } catch (error) {
+      console.error("error");
+      // エラー処理
+    } finally {
+      setIsLoading(false); // ローディングを無効
+    }
   };
 
   return (
     <div>
       <p className="AppSubtitle">家事の管理ページ</p>
-      <div className="workSelect">
-        {formatData.map((work) => (
+      {isLoading && (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="48" height="48">
+        <path fill="#e6edf3"
+              d="M12,0C5.4,0,0,5.4,0,12s5.4,12,12,12,12-5.4,12-12S18.6,0,12,0m0,3c5,0,9,4,9,9s-4,9-9,9S3,17,3,12,7,3,12,3"/>
+        <path fill="#2589d0" d="M12,0c6.6,0,12,5.4,12,12h-3c0-5-4-9-9-9V0Z">
+            <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite"
+                              from="0 12 12" to="360 12 12" dur="1s"/>
+        </path>
+    </svg>
+      )}
+      {!isLoading &&(
+         <><div className="workSelect">
+          {formatData.map((work) => (
             <button
               key={work.value}
               onClick={() => handleWorkClick(work.value)}
@@ -156,49 +204,50 @@ const WorkManage = () => {
               {work.label}
             </button>
           ))}
-      </div>
-      <form>
-      <div>
-          <input
-            placeholder="名前を変更する場合は入力"
-            type="text"
-            className="inputPersonalData"
-            id="username"
-            value={inputWorkname}
-            onChange={(e) => setInputWorkname(e.target.value)} />
-        </div>
-      <div className="frequency">
-          <label className="userSelect">
-            <select value={selectedDate} onChange={handleSelectChange}>
-              <option>何日に</option>
-              {worksfrequency.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              ))}
-            </select>
-        </label>
-        <input
-            placeholder="何回"
-            type="text"
-            className="workcount"
-            value={inputDate}
-            onChange={(e) => setInputDate(e.target.value)} />
-        </div>
-        <div className="buttons">
-          <button 
-          type="button" 
-          className="LoginButton"
-          onClick={handleDeleteSubmit}
-          >削除</button>
-          <button
-            type="button"
-            className="LoginButton"
-            onClick={handleSigninSubmit}
-          >更新
-          </button>
-        </div>
-      </form>
+        </div><form>
+            <div>
+              <input
+                placeholder="名前を変更する場合は入力"
+                type="text"
+                className="inputPersonalData"
+                id="username"
+                value={inputWorkname}
+                onChange={(e) => setInputWorkname(e.target.value)} />
+            </div>
+            <div className="frequency">
+              <label className="userSelect">
+                <select value={selectedDate} onChange={handleSelectChange}>
+                  <option>何日に</option>
+                  {worksfrequency.map((option) => (
+                    <option key={option.value} value={option.value}>
+                      {option.label}
+                    </option>
+                  ))}
+                </select>
+              </label>
+              <input
+                placeholder="何回"
+                type="text"
+                className="workcount"
+                value={inputDate}
+                onChange={(e) => setInputDate(e.target.value)} />
+            </div>
+            <div className="buttons">
+              <button
+                type="button"
+                className="LoginButton"
+                onClick={handleDeleteSubmit}
+              >削除</button>
+              <button
+                type="button"
+                className="LoginButton"
+                onClick={handleSigninSubmit}
+              >更新
+              </button>
+            </div>
+          </form></>
+      )}
+     
     </div>
   );
 };
