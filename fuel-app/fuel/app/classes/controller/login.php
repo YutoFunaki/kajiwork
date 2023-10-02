d915cd02d57cfcabf1f9c971ead9778966bf4a5b
YutoFunaki <kaizuka0629@gmail.com>
2023-10-02 22:20:02 +0900

    localhost:8080でViewを表示する

--- fuel-app/fuel/app/classes/controller/login.php
+++ fuel-app/fuel/app/classes/controller/login.php
@@ -9,7 +9,7 @@ class Login extends \Controller
 
       // CORSヘッダーを設定
       header('Access-Control-Allow-Origin: http://localhost:3000');
-      header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+      header('Access-Control-Allow-Methods: POST');
       header('Access-Control-Allow-Headers: Content-Type, *');
       header('Access-Control-Allow-Credentials: true');
 
@@ -20,6 +20,11 @@ class Login extends \Controller
   }
 
   public function action_index()
+  {
+    return \Response::forge(\View::forge('login/index'));
+  }
+
+  public function action_process()
   {
     if (\Input::method() !== 'POST') {
       return \Response::forge('ログインできませんでした。', 401);
