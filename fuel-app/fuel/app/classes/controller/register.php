c9f74036c8d14bafa184d00e456586c027a8e587
YutoFunaki <kaizuka0629@gmail.com>
2023-10-02 22:20:24 +0900

    メソッドをPOSTのみにする

--- fuel-app/fuel/app/classes/controller/register.php
+++ fuel-app/fuel/app/classes/controller/register.php
@@ -9,7 +9,7 @@ class Register extends \Controller
 
       // CORSヘッダーを設定
       header('Access-Control-Allow-Origin: http://localhost:3000');
-      header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
+      header('Access-Control-Allow-Methods: POST');
       header('Access-Control-Allow-Headers: Content-Type, *');
       header('Access-Control-Allow-Credentials: true');
 
